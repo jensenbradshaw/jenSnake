4745fcddedb0e6c6bdfcb7a99f81b9daac61b291
Jensen Bradshaw <wjb528@york.ac.uk>
2022-05-02 10:28:28 +0100

    Modified the maths for GAME_UNITS

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,7 +8,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	static final int SCREEN_WIDTH = 600;
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 25;
-	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int GAME_UNITS = (SCREEN_WIDTH/UNIT_SIZE)*(SCREEN_HEIGHT/UNIT_SIZE);
 	static final int DELAY = 100;
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
